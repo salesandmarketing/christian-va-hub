cc6ba60750c7e3631d7a7c5a85fb5d8d4458b7be
githubbest729 <153446032+githubbest729@users.noreply.github.com>
2026-02-09 15:30:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
-  base: "/christian-s-va-hub/", 
+  base: "/christian-s-va-hub/",
   server: {
     host: "::",
     port: 8080,
